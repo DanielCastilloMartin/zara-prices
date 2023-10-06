177e42560b10e112aea3dfba0e2541d76b090c6d
Daniel Del Castillo Martin <dcastillo@opensistemas.com>
2023-10-06 13:15:08 +0200

    first commit

---
+++ src/main/java/com/zara/demo/ZaraDemoApplication.java
@@ -0,0 +1,13 @@
+package com.zara.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZaraDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ZaraDemoApplication.class, args);
+	}
+
+}
